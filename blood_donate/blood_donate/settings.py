eaedf8859d6b52fa1f75fd1ca3a4e111223d1565
effattoma <effat35-2146@diu.edu.bd>
2021-02-10 23:42:25 +0600

    User Authentication

--- blood_donate/blood_donate/settings.py
+++ blood_donate/blood_donate/settings.py
@@ -117,7 +117,16 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Messages
+from django.contrib.messages import constants as messages
 
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- blood_donate/blood_donate/urls.py
+++ blood_donate/blood_donate/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('contact/', include('contacts.urls')),
     path('donor/', include('doner.urls')),
     path('add_donor/', include('addDonors.urls')),
+    path('accounts', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
