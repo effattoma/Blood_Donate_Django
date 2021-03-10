bb107885cc54c8d35245776be5ea4063a566ff89
effattoma <effat35-2146@diu.edu.bd>
2021-03-10 11:10:57 +0600

    email

--- blood_donate/blood_donate/settings.py
+++ blood_donate/blood_donate/settings.py
@@ -137,8 +137,8 @@ MESSAGE_TAGS = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'tamannatoma2146@gmail.com'
-EMAIL_HOST_PASSWORD = 'effattamanna09'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 #phone Number verification
